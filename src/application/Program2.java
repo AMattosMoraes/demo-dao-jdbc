278068e9b575dabb681f2d69a5cb6673be421e74
Alexandre Mattos <alemattos_32@hotmail.com>
2025-06-18 15:52:18 -0300

    Insert (Department)

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -5,6 +5,7 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -23,11 +24,20 @@ public class Program2 {
         System.out.println(department);
         System.out.println();
 
-        System.out.println(".:: Teste 2: seller findByAll ::.");
+        System.out.println(".:: Teste 2: Department findByAll ::.");
         List<Department> list = departmentDao.findAll();
         for(Department dep : list){
             System.out.println(dep);
         }
         System.out.println();
+
+        System.out.println(".:: Teste 3: Department insert ::.");
+        Department newdepartment = new Department( null, "Cars");
+        departmentDao.insert(newdepartment);
+        System.out.println("Inserted! New id: " + newdepartment.getId());
+        System.out.println();
+
+
+
     }
 }
