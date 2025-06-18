84b32d76baaa597910f3ff60514996f0dce7d171
Alexandre Mattos <alemattos_32@hotmail.com>
2025-06-18 15:39:51 -0300

    findAll(Department)

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -5,6 +5,7 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Program2 {
@@ -16,12 +17,17 @@ public class Program2 {
         DepartmentDao departmentDao = DaoFactory.createDepartmenteDAO();
 
         System.out.println(".:: Teste 1: Department findById ::.");
-        System.out.print("Enter the Department code: ");
-        int id = sc.nextInt();
-        Department department = departmentDao.findById(id);
+//        System.out.print("Enter the Department code: ");
+//        int id = sc.nextInt();
+        Department department = departmentDao.findById(1);
         System.out.println(department);
         System.out.println();
 
-
+        System.out.println(".:: Teste 2: seller findByAll ::.");
+        List<Department> list = departmentDao.findAll();
+        for(Department dep : list){
+            System.out.println(dep);
+        }
+        System.out.println();
     }
 }
